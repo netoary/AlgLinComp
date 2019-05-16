8c33b4ef0e792858f3c15fd92b2e9ca893825a86
Ary Andrade Neto <aryiff@gmail.com>
2019-05-16 16:43:09 -0300

    interpolacao de Richard

--- functions.f90
+++ functions.f90
@@ -1,11 +1,13 @@
 subroutine f(x, y)
     real x, y
-    y = x*x - 4 * cos(x)
+    !y = x*x - 4 * cos(x)
+    !y = x*x
+    y = sin(x)
 
 end subroutine
 
 subroutine bissecao(tol, a, b)
-    real :: meio, a, b, tol, delta
+    real :: meio, a, b, tol, delta, y
     integer :: i
 
     i = 0
@@ -25,3 +27,44 @@ subroutine bissecao(tol, a, b)
 
 end subroutine
 
+subroutine diferencaCentral(x, deltaX, fLinha)
+    real :: x, deltaX, f1, f2, fLinha
+
+    call f(x+deltaX, f1)
+    call f(x-deltaX, f2)
+
+    fLinha = (f1-f2)/(2*deltaX)
+
+end subroutine
+
+subroutine passoFrente(x, deltaX, fLinha)
+    real :: x, deltaX, f1, f2, fLinha
+
+    call f(x+deltaX, f1)
+    call f(x, f2)
+
+    fLinha = (f1-f2)/(deltaX)
+
+end subroutine
+
+subroutine passoTras(x, deltaX, fLinha)
+    real :: x, deltaX, f1, f2, fLinha
+
+    call f(x, f1)
+    call f(x-deltaX, f2)
+
+    fLinha = (f1-f2)/(deltaX)
+
+end subroutine
+
+subroutine interpolacaoRichard(x, deltaX1, deltaX2, fLinha, p)
+    real :: x, deltaX1, deltaX2, d1, d2, q, fLinha
+
+    q = deltaX1/deltaX2
+
+    call passoFrente(x, deltaX1, d1)
+    call passoFrente(x, deltaX2, d2)
+
+    fLinha = d1 + (d1 - d2)/((q**(-p))-1)
+
+end subroutine
---
+++ lista_7.f90
@@ -0,0 +1,23 @@
+include 'methods.f90'
+include 'functions.f90'
+
+
+program lista_7
+    real :: x, deltaX, deltaX1, deltaX2, fLinha, p
+
+    deltaX = 0.001
+    deltaX1 = 0.5
+    deltaX2 = 0.25
+    x = 1
+    p = 1
+    fLinha = 0
+
+    !call diferencaCentral(x, deltaX, fLinha)
+    !call passoFrente(x, deltaX, fLinha)
+    !call passoTras(x, deltaX, fLinha)
+    call interpolacaoRichard(x, deltaX1, deltaX2, fLinha, p)
+    !call f(b, y)
+
+    write (*,*) fLinha
+
+end program lista_7
